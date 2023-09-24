d8d481d88613ceb298de73b0dc7353bd64fb0505
lolek55507 <110234766+lolek55507@users.noreply.github.com>
2023-09-24 19:14:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    const ball = document.getElementById('ball');
+    const canvasWidth = 1000;
+    const canvasHeight = 600;
+    const playerWidth = 4;
+    const playerHeight = 100;
+    const ballWidth = 50;
+    const ballHeight = 50;
+    let ballCoordsY = canvasHeight / 2 ;
+    let ballCoordsX = canvasWidth / 2 - 25;
+    let leftCoorsY = 300;
+    let rightCoorsY = 300;
+    const playerSpeed = 5;
+    window.addEventListener('keydown', (e) => {
+        if (e.key === "w" && leftCoorsY > 0) {
+            leftCoorsY -= playerSpeed;
+            console.log(leftCoorsY);
+        } else if (e.key === "s" && leftCoorsY < 500) {
+            leftCoorsY += playerSpeed;
+            console.log(leftCoorsY);
+        }
+
+        if (e.key === "ArrowUp"  && rightCoorsY > 0) {
+            rightCoorsY -= playerSpeed;
+        } else if (e.key === "ArrowDown" && rightCoorsY < 500) {
+            rightCoorsY += playerSpeed;
+        }
+    })
+
+
+    function animate() {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillStyle = "white";
+        ctx.fillRect(20, leftCoorsY, playerWidth, playerHeight);
+        ctx.fillRect(970, rightCoorsY, playerWidth, playerHeight);
+        ctx.fillRect(canvasWidth/2, 50, 1, 500);
+        ctx.drawImage(ball, ballCoordsX, ballCoordsY, ballWidth, ballHeight)
+        
+        requestAnimationFrame(animate);
+    }
+    animate()
+})
